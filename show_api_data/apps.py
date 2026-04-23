51fc20f569f3278723b00fb2371d67087decd6c7
Tomas-Kramar-stu <xkramart@stuba.sk>
2026-04-23 15:42:16 +0200

    Delete show_api_data directory

--- show_api_data/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShowApiDataConfig(AppConfig):
-    name = 'show_api_data'
-    def ready(self):
-
-        from sceduler import  sceduler
-        sceduler.start()
